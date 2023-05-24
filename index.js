89fb0e45e396c37844d93c6ae86804829ddd3c80
boudewijn overvliet <boudewijn@hello-data.nl>
2023-05-24 14:52:30 +0200

    wip

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('<h1>Hello World!</h1>');
+  res.write('<h1>Hello World!!!</h1>');
   res.end();
 }).listen(80);
 
